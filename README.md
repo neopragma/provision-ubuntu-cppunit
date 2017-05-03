4d7c58d9185ac42fbe1bef808341becfe6808fdb
Dave Nicolette <davenicolette@gmail.com>
2017-05-03 13:19:26 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # provision-ubuntu-cppunit
+
+This is a scripted install of CppUnit for Ubuntu Desktop instances.
+
+1. Clone this repo.
+2. cd to the repo directory.
+3. run ```./setup```.
+
---
+++ cppunit_sample/MoneyTest.h
@@ -0,0 +1,25 @@
+#ifndef MONEYTEST_H
+#define MONEYTEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+#include "Money.h"
+
+class MoneyTest : public CppUnit::TestFixture
+{
+  CPPUNIT_TEST_SUITE( MoneyTest );
+  CPPUNIT_TEST( testConstructor );
+  CPPUNIT_TEST( testEqual );
+  CPPUNIT_TEST( testAddThrow );
+ // CPPUNIT_TEST_EXCEPTION( testAddThrow, IncompatibleMoneyError );
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void setUp();
+  void tearDown();
+  void testConstructor();
+  void testEqual();
+  void testAdd();
+  void testAddThrow();
+};
+
+#endif  // MONEYTEST_H
